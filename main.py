55e5f4dfa312a3cc54cb7e5201c6fae311e0423b
andresfhortua <32169787+andresfhortua@users.noreply.github.com>
2020-12-17 14:20:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI, HTTPException
 import db
+from fastapi.middleware.cors import CORSMiddleware
 
-api = FastAPI()
+app = FastAPI()
+
+origins = [
+ "http://localhost:8080"
+]
+app.add_middleware(
+CORSMiddleware, 
+allow_origins=origins,
+allow_credentials=True, 
+allow_methods=["*"], 
+allow_headers=["*"],
+)
 
 @api.get("/documentos/")
 async def obtener_docs():
